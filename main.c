cf2ca3da9b3d80e5755e5886f212ca8dd0d252cc
Brook-Hsu <55443872+Brook-hsu@users.noreply.github.com>
2020-01-27 17:01:18 +0800

    完成打印函数

--- main.c
+++ main.c
@@ -1,43 +1,74 @@
-/*摆脱无聊的尝试（刷子已完成任务，剩下的交给纯哥了）*/
+/*�������ĵĳ���*/
 
-///游戏2048工作台
-///程序步骤
-///1.打印界面（每次打印要有刷新）
-///2.初始化变量
-///3.键入方向键
-///4.对应操作
-///5.随机位置生成新数
-///6.记分数
-///7.判断游戏是否结束（是：结束；否：回到3）
+///��Ϸ2048����̨
+///������
+///1.��ӡ���棨ÿ�δ�ӡҪ��ˢ�£�
+///2.��ʼ������
+///3.���뷽���
+///4.��Ӧ����
+///5.���λ����������
+///6.�Ƿ���
+///7.�ж���Ϸ�Ƿ�������ǣ��������񣺻ص�3��
 
-///合作方法：各自完成部分函数，统一变量命名，拿自己喜欢（会写的）的部分写
+///����������������ɲ��ֺ�����ͳһ�������������Լ�ϲ������д�ģ��Ĳ���д
 
-///代码有问题找百度，找刷子同学也行，他自己也不会，不过可以发给他确认（嘲笑）他不会
+///�����������Ұٶȣ���ˢ��ͬѧҲ�У����Լ�Ҳ���ᣬ�������Է�����ȷ�ϣ���Ц��������
 #include <stdio.h>
 #include <stdlib.h>
-#include<time.h>
-///定义全局变量4*4个，从num[1][1]开始
-int num[5][5];
-///自定义函数    初步定义函数都为void，可能后续有些要变为int
-/*打印界面*/
+///����ȫ�ֱ���4*4������num[1][1]��ʼ
+int num[5][5],Line=4,Col=4;
+///�Զ��庯��    �������庯����Ϊvoid�����ܺ�����ЩҪ��Ϊint
+/*��ӡ����*/
 void Print();
-/*初始化变量*/
+/*��ʼ������*/
 void Initial();
-/*键入方向键*////可能要百度解决
+/*���뷽���*////����Ҫ�ٶȽ��
 void Control();
     void Turn_left();
     void Turn_right();
     void Turn_up();
     void Turn_down();
-/*生成随机数*////用rand()种子
+/*���������*////��rand()����
 void New_random();
-/*判断游戏是否结束*/
+/*�ж���Ϸ�Ƿ����*/
 void Is_over();
 
 
-///main函数作为主体，视图要一目了然，代码量不要太多，主要作用是引入自定义函数
+///main������Ϊ���壬��ͼҪһĿ��Ȼ����������Ҫ̫�࣬��Ҫ�����������Զ��庯��
 int main()
 {
-    printf("Hello world!\n");
+    Print();
     return 0;
+}
+
+void Print(){
+    int i,j;
+    ///Head
+    printf("�X");
+    for(j=1;j<=Col;j++){
+        printf("�T�T�T�T");
+        if(Col-j)   printf("�j");
+    }
+    printf("�[\n");
+    ///body
+    for(i=1;i<=Line;i++){
+        for(j=1;j<=Col;j++){
+            printf("�U%4d",num[i][j]);
+        }
+        printf("�U\n");
+        if(i-Line){
+        for(j=1;j<=Col;j++){
+            printf("�U�T�T�T�T");
+        }
+        printf("�U\n");}
+    }
+    ///foot
+    printf("�^");
+    for(j=1;j<=Col;j++){
+        printf("�T�T�T�T");
+        if(Col-j)   printf("�m");
+    }
+    printf("�a");
+
+
 }
